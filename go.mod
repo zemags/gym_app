cac6536f5e6b4d9a88f22278d7a82ad75c1eda65
zemags <azmt2008@gmail.com>
2021-05-01 11:33:19 +0500

    Add conn for db, configs

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.7.1
+	github.com/jmoiron/sqlx v1.3.3
+	github.com/mattn/go-sqlite3 v1.14.7
 	github.com/spf13/viper v1.7.1
 )
--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -1,5 +1,7 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Authorization interface {
 }
 
@@ -7,6 +9,6 @@ type Repository struct {
 	Authorization
 }
 
-func NewRepository() *Repository {
+func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{}
 }
